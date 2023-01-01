cd4c0522cbde1ef101356df4fa1640b3c3ff98a3
Samer MACHARA <smachara@gmail.com>
2023-01-01 08:56:40 +0100

    Change ApiContext namespace.

--- src/shared/infrastructure/Behat/ApiContext.php
+++ src/shared/infrastructure/Behat/ApiContext.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace MacharaM\Tests\Shared\Infrastructure\Behat;
+namespace MacharaM\Shared\Infrastructure\Behat;
 
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
