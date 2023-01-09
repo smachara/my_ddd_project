9f8b95bbdbe8e6ab6f72223fc2019bfa06fc0956
Samer MACHARA <smachara@gmail.com>
2023-01-09 11:44:50 +0100

    Example of bussines rule "The name should have more than 3 characters"

--- src/Social/Member/Domain/ValueObject/MemberName.php
+++ src/Social/Member/Domain/ValueObject/MemberName.php
@@ -7,5 +7,16 @@ use MacharaM\Shared\Domain\ValueObject\StringValueObject;
 
 class MemberName extends StringValueObject
 {
-    
+    public function __construct(string $value) {
+        $this->ensure_min_lenght($value);
+        parent::__construct($value);
+    }
+
+    private function ensure_min_lenght($value)
+    {
+        $minlenght = 3;
+        if (strlen($value)<$minlenght) {
+            throw new \InvalidArgumentException("The name should have more than 3 characters");
+        }
+    }
 }
